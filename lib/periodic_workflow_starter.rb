9b50d5095fa20e0708a071d0801eae106081a1e9
Moustafa Aly <moustafa@amazon.com>
2015-01-18 16:32:22 -0800

    Adjusting configuration

--- lib/periodic_workflow_starter.rb
+++ lib/periodic_workflow_starter.rb
@@ -14,11 +14,41 @@
 ##
 
 require 'aws/decider'
+require 'json'
 require_relative 'utils'
 require_relative 'periodic_workflow'
 include AWS::Flow
 
-my_workflow_client = workflow_client($swf.client, $domain) { {:from_class => "PeriodicWorkflow"} }
+
+
+def setup_domain(domain_name, retention_period)
+swf = AWS::SimpleWorkflow.new
+domain = swf.domains[domain_name]
+unless domain.exists?
+swf.domains.create(domain_name, retention_period)
+end
+domain
+end
+# get the path to the runner configuration file.
+if ARGV.length < 1
+puts "Please provide the path to the runner configuration file!"
+exit
+end
+runner_spec = ARGV[0]
+# Read the domain info from the same JSON file that the runner will be using.
+helloworld_options = JSON.parse(File.read(runner_spec))
+domain = helloworld_options["domain"]
+domain_name = domain["name"]
+# If retention is not given, default it to 1
+retention_period = domain["retention_in_days"] || 1
+# For this test, use the first workflow encountered.
+workflow_info = helloworld_options["workflow_workers"][0]
+task_list_name = workflow_info["task_list"]
+domain = setup_domain(domain_name, retention_period)
+
+
+
+my_workflow_client = workflow_client($swf.client, domain) { {:from_class => "PeriodicWorkflow"} }
 
 puts "starting an execution..."
 running_options = PeriodicWorkflowOptions.new(10,false,20,40)
