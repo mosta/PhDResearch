f68598ded45b80d8d5d951df098d1758faca9bce
HanySalahEldeen <hany@cs.odu.edu>
2015-02-04 14:40:31 -0500

    v17

--- lib/periodic_activity.rb
+++ lib/periodic_activity.rb
@@ -20,7 +20,7 @@ class PeriodicActivity
 
   activity :do_some_work do
     {
-      :version => "16.0",
+      :version => "17.0",
       :default_task_list => $activity_task_list,
       :default_task_schedule_to_start_timeout => 36000,
       :default_task_start_to_close_timeout => 36000,
