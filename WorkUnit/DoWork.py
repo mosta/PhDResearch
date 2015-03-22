e984aa09752b211f9052e8cbd6bbc0d43e875d36
HanySalahEldeen <hany@cs.odu.edu>
2015-03-22 19:19:03 -0400

    setting timeout for s3

--- WorkUnit/DoWork.py
+++ WorkUnit/DoWork.py
@@ -11,6 +11,10 @@ import io, json
 import calendar
 import time
 from timeout import timeout
+  
+import boto
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
 
 @timeout(60)
 def getTopsyTweets(path, ID, URL, timestamp):
@@ -50,43 +54,43 @@ def getSnapshotofURL(path, ID, URL, timestamp):
 
 
 
-timenow = str(calendar.timegm(time.gmtime()))
+@timeout(300)
+def work(ID, URL):
+	timenow = str(calendar.timegm(time.gmtime()))
+	path = "/srv/www/phdresearch/CollectedData/"+ID+"/"
+	if(os.path.exists(path)==False):
+		commands.getoutput("mkdir "+path)	      
+
+	json_data=open('/srv/www/phdresearch/awscredentials_s3.txt')
+	data = json.load(json_data)
+	json_data.close()
+	S3connection = S3Connection(data["aws_key"],data["aws_secret"])
+	S3bucket = S3connection.get_bucket('longitudinalstudy.v3')
+
+	try:
+		getSnapshotofURL(path, ID, URL, timenow)
+		#topsyFileName = ID+"_"+timenow+".topsy"
+		#getTopsyTweets(path, ID, URL,timenow)
+		htmlFileName = ID+"_"+timenow+".html"
+		pngFileName = ID+"_"+timenow+".png"
+	
+		#newfile = Key(S3bucket)
+		#newfile.key = topsyFileName
+		#newfile.set_contents_from_filename(path+topsyFileName)
+		newfile = Key(S3bucket)
+		newfile.key = htmlFileName
+		newfile.set_contents_from_filename(path+htmlFileName)
+		newfile = Key(S3bucket)
+		newfile.key = pngFileName
+		newfile.set_contents_from_filename(path+pngFileName)
+	
+		#commands.getoutput("rm -f "+path+topsyFileName)
+		commands.getoutput("rm -f "+path+htmlFileName)
+		commands.getoutput("rm -f "+path+pngFileName)
+	except:
+		pass
+
+
 ID = sys.argv[1]
 URL = sys.argv[2]
-path = "/srv/www/phdresearch/CollectedData/"+ID+"/"
-if(os.path.exists(path)==False):
-        commands.getoutput("mkdir "+path)
-        
-import boto
-from boto.s3.connection import S3Connection
-from boto.s3.key import Key
-
-json_data=open('/srv/www/phdresearch/awscredentials_s3.txt')
-data = json.load(json_data)
-json_data.close()
-S3connection = S3Connection(data["aws_key"],data["aws_secret"])
-S3bucket = S3connection.get_bucket('longitudinalstudy.v2')
-
-try:
-	getSnapshotofURL(path, ID, URL, timenow)
-	#topsyFileName = ID+"_"+timenow+".topsy"
-	#getTopsyTweets(path, ID, URL,timenow)
-	htmlFileName = ID+"_"+timenow+".html"
-	pngFileName = ID+"_"+timenow+".png"
-	
-	#newfile = Key(S3bucket)
-	#newfile.key = topsyFileName
-	#newfile.set_contents_from_filename(path+topsyFileName)
-	newfile = Key(S3bucket)
-	newfile.key = htmlFileName
-	newfile.set_contents_from_filename(path+htmlFileName)
-	newfile = Key(S3bucket)
-	newfile.key = pngFileName
-	newfile.set_contents_from_filename(path+pngFileName)
-	
-	#commands.getoutput("rm -f "+path+topsyFileName)
-	commands.getoutput("rm -f "+path+htmlFileName)
-	commands.getoutput("rm -f "+path+pngFileName)
-except:
-	pass
-
+work(ID, URL)
